2efbd0f4e6c9b8a743d4f594d026d842c64ad2e9
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-07-31 22:42:17 +0530

    src: added site config

--- dokeshi/generator.go
+++ dokeshi/generator.go
@@ -1,7 +1,10 @@
 package dokeshi
 
 import (
+	"fmt"
 	"html/template"
+	"path/filepath"
+	"sort"
 	"time"
 )
 
@@ -34,4 +37,52 @@ type Generator interface {
 
 type SiteGenerator struct {
 	Config *SiteConfig
+}
+
+// siteconfig holds the sources and destination folder
+type SiteConfig struct {
+	Sources     []string
+	Destination string
+	Config      *Config
+}
+
+// create a new sitegenerator
+func New(config *SiteConfig) *SiteGenerator {
+	return &SiteGenerator{Config: config.Config}
+}
+
+// generate the start of the static blog
+func (g *SiteGenerator) Generate() error {
+	templatePath := filepath.Join("static", "template.html")
+	fmt.Println("Generating Site 🏃‍♂️🏃‍♂️🏃‍♂️")
+	sources := g.Config.Sources
+	destination := g.Config.Destination
+
+	if err := clearAndCreateDestination(destination); err != nil {
+		return err
+	}
+
+	if err := clearAndCreateDestination(filepath.Join(destination, "archive")); err != nil {
+		return err
+	}
+
+	t, err := getTemplate(templatePath)
+	if err != nil {
+		return err
+	}
+
+	var posts []*Post
+	for _, path := range sources {
+		post, err := newPost(path, g.Config.Config.Blog.Datefmt)
+		if err != nil {
+			return err
+		}
+		posts = append(posts, post)
+	}
+	sort.Sort(ByDateDesc(posts))
+	if err := runTasks(posts, t, destination, g.Config.Config); err != nil {
+		return err
+	}
+	fmt.Println("⚡️ Finished generating the site.")
+	return nil
 }

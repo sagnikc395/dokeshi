9287db6486f3bcba625aa6398d68349e4d010c18
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-01 01:41:53 +0530

    src: added generate and generateTagindex

--- dokeshi/tags.go
+++ dokeshi/tags.go
@@ -1,7 +1,10 @@
 package dokeshi
 
 import (
+	"bytes"
 	"fmt"
+	"path/filepath"
+	"sort"
 	"strings"
 	"text/template"
 )
@@ -36,6 +39,56 @@ func createTag(tags []string) []*Tag {
 		result = append(result, &Tag{Name: tag, Link: getTagLink(tag)})
 	}
 	return result
+}
+
+func (g *TagsGenerator) Generate() error {
+	fmt.Println("\tGenerating tags...")
+	tagPostsMap := g.Config.TagPostsMap
+	t := g.Config.Template
+	destination := g.Config.Destination
+	tagsPath := filepath.Join(destination, "tags")
+	if err := clearAndCreateDestination(tagsPath); err != nil {
+		return err
+	}
+
+	if err := generateTagIndex(tagPostsMap, t, tagsPath, g.Config.Writer); err != nil {
+		return err
+	}
+
+	for tag, tagPosts := range tagPostsMap {
+		tagPagePath := filepath.Join(tagsPath, tag)
+		if err := generateTagIndex(tag, tagPosts, t, tagPagePath, g.Config.Writer); err != nil {
+			return err
+		}
+	}
+	fmt.Println("\tFinished generating tags...")
+	return nil
+}
+
+func generateTagIndex(tagPostsMap map[string][]*Post, t *template.Template, destination string, writer *IndexData) error {
+	tagsTemplatePath := filepath.Join("static", "tags.html")
+	tmpl, err := getTemplate(tagsTemplatePath)
+
+	if err != nil {
+		return err
+	}
+
+	tags := []*Tag{}
+
+	for tag, posts := range tagPostsMap {
+		tags = append(tags, &Tag{Name: tag, Link: getTagLink(tag), Count: len(posts)})
+	}
+
+	sort.Sort(ByCountDesc(tags))
+	buf := bytes.Buffer{}
+	if err := tmpl.Execute(&buf, tags); err != nil {
+		return fmt.Errorf("error executing template %s: %v", tagsTemplatePath, err)
+	}
+	if err := writer.WriteIndexHTML(destination, "Tags", "Tags", template.HTML(buf.String()), t, ""); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func generateTagPage(tag string, posts []*Post, t *template.Template,
